65245293cfd847b99846563e353b83cabaa14adb
Sømjo <106537238+somjonn@users.noreply.github.com>
2022-07-22 18:27:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const image = document.querySelector("img");
+const button = document.querySelector("button");
+
+
+window.onload = () => generateRandomPicture(imageArray);
+
+button.addEventListener("click", () => generateRandomPicture(imageArray));
+
+function generateRandomPicture(array){
+	let randomNum = Math.floor(Math.random() * array.length); 
+	image.setAttribute("src", array[randomNum]);
+}
